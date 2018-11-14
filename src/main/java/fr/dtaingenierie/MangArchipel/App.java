8c206463febc8ad20b0ab82357cf8f071252e5c3
Samy Chaabi <xhackax47@gmail.com>
2018-11-14 13:39:38 +0100

    First

---
+++ src/main/java/fr/dtaingenierie/MangArchipel/App.java
@@ -0,0 +1,13 @@
+package fr.dtaingenierie.MangArchipel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
