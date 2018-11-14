ed242bb83058499bceaa0a97a2048330d0df116c
Samy Chaabi <xhackax47@gmail.com>
2018-11-14 13:51:31 +0100

    Maven project multi-modules

---
+++ MangArchipelBack/src/main/java/MangArchipelBack/App.java
@@ -0,0 +1,13 @@
+package MangArchipelBack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
