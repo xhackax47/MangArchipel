bcd611fb08203fdbca08f87877bd9b24fffe4269
florent <florenzabs@hotmail.fr>
2018-11-15 16:23:25 +0100

    vue adminstrateur réalisé + une modification de la vue utilisateur

--- MangArchipelFront/MangArchipelAngular/src/app/app.module.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppComponent } from './app.component';
 import { MenuBarDetailsComponent } from './menu-bar-details/menu-bar-details.component';
 
 import { MenubarModule } from 'primeng/menubar';
+import { MenuAdministrateurComponent } from './menu-administrateur/menu-administrateur.component';
 
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarDetailsComponent
+    MenuBarDetailsComponent,
+    MenuAdministrateurComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MenubarModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
