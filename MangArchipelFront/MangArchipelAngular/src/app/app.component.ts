404583e4a1b2bb2e5f78647d73338f8cebd43e93
Bruno Murat <sharudowii@hotmail.com>
2018-11-20 11:34:49 +0100

    conflit package

--- MangArchipelFront/MangArchipelAngular/src/app/app.component.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import { Component } from '@angular/core';
 
@@ -18,24 +18,3 @@ export class AppComponent {
   }
 }
 
-=======
-
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'MangArchipel';
-  logged: boolean;
-  admin: boolean;
-
-  constructor() {
-    this.logged = false;
-    this.admin = false;
-  }
-}
-
->>>>>>> 2d71d9694e7bced0f656b66eef36dc578fc37a1f
