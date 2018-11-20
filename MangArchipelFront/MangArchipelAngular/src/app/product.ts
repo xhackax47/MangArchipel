f085b61ce82e7421d206b1fa3b9031c90a76e43b
unknown <oudin.angel@gmail.com>
2018-11-20 14:36:47 +0100

    Css produit detail

--- MangArchipelFront/MangArchipelAngular/src/app/product.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/product.ts
@@ -6,14 +6,17 @@ export class Product {
     price: number;
     productType: string;
     description: string;
+    image: string;
 
     constructor(name?: string, brand?: string,
-        price?: number, type?: string, description?: string) {
+        price?: number, type?: string, description?: string,
+        image?: string) {
         this.productName = name;
         this.brand = brand;
         this.price = price;
         this.productType = type;
         this.description = description;
+        this.image = image;
     }
 }
 
