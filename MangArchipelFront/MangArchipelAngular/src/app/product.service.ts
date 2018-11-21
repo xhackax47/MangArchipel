92a771957db296bc3a4f660d3b82d896ed46c477
unknown <oudin.angel@gmail.com>
2018-11-21 17:44:56 +0100

    Mise en place d ela recherche en cours

--- MangArchipelFront/MangArchipelAngular/src/app/product.service.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/product.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Product } from './product';
 import { Observable } from 'rxjs';
 
@@ -11,33 +11,46 @@ export class ProductService {
 
   url = 'http://localhost:8098/api/products';
   httpOptions = {
-    headers: new HttpHeaders().set('Content-type', 'application/json')
+    headers: new HttpHeaders().set('Content-type', 'application/json'),
+    params: new HttpParams()
   };
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<Array<Product>> {
-    return this.http.get<Array<Product>>(this.url + '/', this.httpOptions);
+    return this.http.get<Array<Product>>(this.url + '/');
   }
 
   deleteProduct(id: number) {
-    this.http.delete(this.url + '/' + id, this.httpOptions);
+    this.http.delete(this.url + '/' + id);
   }
 
   getProduct(id: number): Observable<Product> {
-    return this.http.get<Product>(this.url + '/' + id, this.httpOptions);
+    return this.http.get<Product>(this.url + '/' + id);
   }
 
   getProductsById(id: Number): Observable<Product> {
     return this.http.get<Product>(this.url + id);
   }
 
-  getProductBy(arrayProduct: Product[]): Observable<Product> {
-    return this.http.get<Product>(this.url + '/' + arrayProduct);
-  }
+  // getProductBy(arrayProduct: Product[]): Observable<Product> {
+  //   return this.http.get<Product>(this.url + '/' + arrayProduct);
+  // }
 
   addProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.url + '/', product, this.httpOptions);
+    return this.http.post<Product>(this.url + '/', product);
   }
 
+  productFilterName(name?: string): Observable<Array<Product>>  {
+    return this.http.get<Array<Product>>(this.url + '/',
+      { headers: this.httpOptions.headers, params: {
+        productName: name
+      }});
+  }
+  productFilterBrand(brand?: string): Observable<Array<Product>>  {
+    return this.http.get<Array<Product>>(this.url + '/',
+      { headers: this.httpOptions.headers, params: {
+        brand: brand
+      }});
+  }
 }
--- MangArchipelFront/MangArchipelAngular/src/app/search-product/search-product.component.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/search-product/search-product.component.ts
@@ -2,23 +2,27 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
+import { HttpResponse } from '@angular/common/http';
 @Component({
   selector: 'app-search-product',
   templateUrl: './search-product.component.html',
   styleUrls: ['./search-product.component.css']
 })
 export class SearchProductComponent implements OnInit {
-
+  product: Product;
   checkValue: any;
-  productArray: Array<Product>;
+  productArray;
+
   constructor(private service: ProductService) {
     this.productArray = [];
-   }
+  }
 
   ngOnInit() {
     // this.service.getProductsById();
   }
   onSubmit() {
-    this.service.getProductBy(this.productArray).subscribe();
+    // this.service.getProductBy(this.productArray).subscribe();
+    console.log('toto');
+    this.service.productFilterName(this.checkValue).subscribe(p => this.productArray = p);
   }
 }
