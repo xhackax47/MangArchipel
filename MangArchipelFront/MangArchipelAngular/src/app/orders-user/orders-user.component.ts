2966e3a20957f308ccd09f51ca36c401adab7446
Flamboyant <murat.bruno35@gmail.com>
2018-11-27 00:25:55 +0100

    bouton detail

--- MangArchipelFront/MangArchipelAngular/src/app/orders-user/orders-user.component.ts
+++ MangArchipelFront/MangArchipelAngular/src/app/orders-user/orders-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Order } from '../order';
 import { OrderService } from '../order.service';
 import { ProductOrder } from '../product-order';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders-user',
@@ -10,16 +10,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./orders-user.component.css']
 })
 export class OrdersUserComponent implements OnInit {
-  orders: ProductOrder[];
+  ProductOrderArray: ProductOrder[];
+  Orders: Order[];
 
-  constructor( private orderService: OrderService,private route: ActivatedRoute) {
+  constructor(private orderService: OrderService, private route: ActivatedRoute, private router: Router) {
     const id = parseInt(this.route.snapshot.paramMap.get('id'), 0);
-    this.orderService.getOrdersByUserId(id).subscribe(orders => {
-      this.orders = orders;
+    this.orderService.getOrdersByUserId(id).subscribe(Orders => {
+      this.Orders = Orders;
     });
-   }
+  }
 
   ngOnInit() {
+  }
+
+  details(id:number){
+    this.router.navigate(['/']);
   }
 
 }
